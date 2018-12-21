7b0a39684ae5c17d3a91f90c841dd92d54eeee0b
tyw66 <2639144635@qq.com>
2018-12-21 16:41:37 +0800

    添加鼠标点击拖拽交互功能

--- gui/viewer.cpp
+++ gui/viewer.cpp
@@ -1,6 +1,7 @@
 #include "viewer.h"
 #include <QDebug>
 #include <QMimeData>
+#include <QMessageBox>
 #include "../core/parser/objfileparser.h"
 
 unsigned int Viewer::m_time = 0;
@@ -16,6 +17,7 @@ Viewer::Viewer(QWidget *parent) : QWidget(parent)
     m_image = new QImage(W,H,QImage::Format_RGB32);
     m_scene = new Scene();
 
+    m_status= FREE;
     m_scene->m_mesh.makeCube(); //程序加载时画立方体
 
 
@@ -25,13 +27,13 @@ Viewer::Viewer(QWidget *parent) : QWidget(parent)
 
 void Viewer::renderImg(int x, int y, int x0, int y0)
 {
-    temp_timer.restart();
+    fps_timer.restart();
 
+    //模型变换
     m_scene->update((double)x/W, (double)y/H ,(double)x0/W, (double)y0/H);
 
-#pragma omp parallel for
-    //遍历像素点
-    for(int i = 0; i < H; ++i){
+    //#pragma omp parallel for
+    for(int i = 0; i < H; ++i){    //遍历像素点
         for(int j = 0; j < W; ++j){
             //获取颜色
             Color color = sample((double)j/W, (double)i/H, m_scene);
@@ -40,20 +42,7 @@ void Viewer::renderImg(int x, int y, int x0, int y0)
         }
     }
     update();
-    //    m_time++;
-
-    //    qDebug()<< temp_timer.elapsed();
-}
-
-void Viewer::mouseMoveEvent(QMouseEvent *ev)
-{
-    mouse_curX = ev->x();
-    mouse_curY = ev->y();
-
-    renderImg(mouse_curX, mouse_curY,mouse_posX, mouse_posY);
-
-    mouse_posX = mouse_curX;
-    mouse_posY = mouse_curY;
+    qDebug()<< fps_timer.elapsed();
 }
 
 void Viewer::paintEvent(QPaintEvent *ev)
@@ -66,10 +55,13 @@ void Viewer::paintEvent(QPaintEvent *ev)
 void Viewer::dragEnterEvent(QDragEnterEvent *ev)
 {
     //如果类型是obj才能接受拖动。否则不接受鼠标事件
-    if(!ev->mimeData()->urls()[0].fileName().right(3).compare("obj")) //compare相等的时候返回0
+    if(!ev->mimeData()->urls()[0].fileName().right(3).compare("obj")){ //compare相等的时候返回0
         ev->acceptProposedAction();
-    else
+    }
+    else{
         ev->ignore();
+        QMessageBox::information(this, tr("Information"), tr("The file is not obj file."));
+    }
 }
 
 void Viewer::dropEvent(QDropEvent *ev)
@@ -78,4 +70,31 @@ void Viewer::dropEvent(QDropEvent *ev)
     ObjFileParser::setFilePath(path.toStdString());
     ObjFileParser::parse(m_scene->m_mesh);
 }
+
+
+
+void Viewer::mouseMoveEvent(QMouseEvent *ev)
+{
+    if(m_status == ROTATE){
+        mouse_curX = ev->x();
+        mouse_curY = ev->y();
+        renderImg(mouse_curX, mouse_curY,mouse_posX, mouse_posY);
+        mouse_posX = ev->x();
+        mouse_posY = ev->y();
+    }
+}
+
+void Viewer::mousePressEvent(QMouseEvent *ev)
+{
+    m_status = ROTATE;
+    mouse_posX = ev->x();
+    mouse_posY = ev->y();
+}
+
+void Viewer::mouseReleaseEvent(QMouseEvent *ev)
+{
+    m_status = FREE;
+}
+
+
 
--- gui/viewer.h
+++ gui/viewer.h
@@ -18,6 +18,16 @@
 class Viewer : public QWidget
 {
     Q_OBJECT
+
+    /**
+     * @brief 交互模式
+     */
+    enum INTER_STATUS{
+        FREE = 0,
+        ROTATE = 1,
+        PAN = 2
+    };
+
 public:
     /**
      * @brief 构造函数
@@ -36,39 +46,52 @@ public:
 
 protected:
     /**
-     * @brief 处理鼠标移动事件
-     * @param ev
-     */
-    void mouseMoveEvent(QMouseEvent* ev);
-    /**
      * @brief 处理重绘事件
      * @param ev
      */
     void paintEvent(QPaintEvent* ev );
     /**
-     * @brief dragEnterEvent
+     * @brief 处理拖拽事件
      * @param ev
      */
     void dragEnterEvent(QDragEnterEvent* ev);
     /**
-     * @brief dropEvent
+     * @brief 处理放下事件
      * @param ev
      */
     void dropEvent(QDropEvent* ev);
+    /**
+     * @brief 处理鼠标移动事件
+     * @param ev
+     */
+    void mouseMoveEvent(QMouseEvent* ev);
+    /**
+     * @brief 处理鼠标按下事件
+     * @param ev
+     */
+    void mousePressEvent(QMouseEvent* ev);
+    /**
+     * @brief 处理鼠标释放事件
+     * @param ev
+     */
+    void mouseReleaseEvent(QMouseEvent* ev);
+
+
 
 private:
     const int W = 256;      /**< 图像宽度*/
     const int H = 256;      /**< 图像高度*/
-    QImage* m_image;    /**< 图像数据*/
+    QImage* m_image;        /**< 图像数据*/
+    Scene *m_scene;         /**< 场景数据 */
 
-    static unsigned int m_time;         /**< 时间变量，动画用*/
+    INTER_STATUS m_status;          /**< 交互模式 */
     int mouse_curX, mouse_curY;     /**< 鼠标当前位置*/
     int mouse_posX, mouse_posY;     /**< 鼠标上一位置*/
 
-    QTime  temp_timer;          //测试用
+    QTime  fps_timer;                   /**< 测试用 */
+    static unsigned int m_time;         /**< 时间变量，动画用*/
 
 
-    Scene *m_scene;
 
 };
 
